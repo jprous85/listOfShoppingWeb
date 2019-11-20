9c75c5c9dbc8ce8d9705b66c7e95f194b687fe80
jprous85 <jprous85@gmail.com>
2019-11-20 01:41:22 +0100

    navbar bug fix to resize window

--- public/js/custom_app.js
+++ public/js/custom_app.js
@@ -1,3 +1,18 @@
 $(document).ready(()=>{
-    // TODO::for anything make it.
+
+    modifyWindow();
+    $(window).on('load scroll resize', ()=>{
+        modifyWindow();
+    });
+    function modifyWindow() {
+        let wd = window.screen.width;
+        if (wd >= 576) {
+            $('#hr-navbar-item').hide();
+            $('#link-navbar-item').show();
+        }
+        else {
+            $('#hr-navbar-item').show();
+            $('#link-navbar-item').hide();
+        }
+    }
 });
--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-sm navbar-light">
+<nav class="navbar navbar-expand-sm navbar-light navbar-border-light" id="main-navbar">
     <div class="container">
-        <a href="" id="burger-btn"><span><i class="fas fa-bars"></i></span></a>
+        <a href="" class="text-title-navbar">List of Shopping</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -24,22 +24,28 @@
                         </li>
                     @endif
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            {{ ucfirst(Auth::user()->name) }} <span class="caret"></span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="navbar-text text-muted" id="link-navbar-item">/</a>
+                        <hr id="hr-navbar-item">
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();"
+                        >
+                            {{ __('Logout') }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 @endguest
             </ul>
